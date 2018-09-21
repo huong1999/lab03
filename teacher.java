70c825d488af6e21e989f4a7f78cc48bc94ba011
huong1999 <37472308+huong1999@users.noreply.github.com>
2018-09-22 01:32:40 +0700

    Add files via upload

---
+++ teacher.java
@@ -0,0 +1,101 @@
+package bai3;
+public class Teacher {
+
+    private String mon;
+    public void setMon(String mon){
+        this.mon=mon;
+    }
+    public String getMon(){
+        return mon;
+    }
+}
+public class Student{
+    private String truong;
+
+    public void setStudent(String truong){
+        this.truong=truong;
+    }
+    public String getStudent(){
+        return truong;
+    }
+}
+public class Programer{
+    private String congTi;
+
+    public void setCongTi(String congTi){
+        this.congTi=congTi;
+    }
+    public String getCongTi(){
+        return congTi;
+    }
+}
+public class Cat{
+    private String giong;
+
+    public void setGiong(String giong){
+        this.giong=giong;
+    }
+    public String getGiong(){
+        return giong;
+    }
+}
+public class Dog{
+    private String color;
+
+    public void setColor(String color){
+        this.color=color;
+    }
+    public String getColor(){
+        return color;
+    }
+}
+public class Boss{
+    private String linhVuc;
+
+    public void setLinhVuc(String linhVuc){
+        this.linhVuc=linhVuc;
+    }
+    public String getLinhVuc(){
+        return linhVuc;
+    }
+}
+public class Doctor{
+    private String khoa;
+
+    public void setKhoa(String khoa){
+        this.khoa=khoa;
+    }
+    public String getKhoa(){
+        return khoa;
+    }
+}
+public class President{
+    private String country;
+
+    public void setCountry(String country){
+        this.country=country;
+    }
+    public String getCountry(){
+        return country;
+    }
+}
+public class Engineers{
+    private String Nganh;
+
+    public void setNganh(String Nganh){
+        this.Nganh=Nganh;
+    }
+    public String getNganh(){
+        return Nganh;
+    }
+}
+public class Driver{
+    private String phuongTien;
+
+    public void setPhuongTien(String phuongTien){
+        this.phuongTien=phuongTien;
+    }
+    public String getPhuongTien(){
+        return phuongTien;
+    }
+}
